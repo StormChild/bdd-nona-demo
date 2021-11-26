abd1264b2c7e4bda47d0548f0fbfd97c62b86ba9
Ruan Meyer <ragnarr@gmail.com>
2021-11-26 10:16:16 +0200

    Added export to Cucumber.io

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,7 @@ let common = [
   'src/features/**/*.feature',
   '--require-module ts-node/register',
   '--require src/step-definitions/**/*.ts',
-  '--format progress-bar',
+  '--format progress',
   //'--format node_modules/cucumber-pretty'
   '--format json:output/test-results.json'
 ].join(' ');
