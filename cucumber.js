c25113ed164e86dc309b5402822b3fec4a4696e2
Ruan Meyer <ragnarr@gmail.com>
2021-11-26 10:09:13 +0200

    Added json test output

--- cucumber.js
+++ cucumber.js
@@ -3,7 +3,8 @@ let common = [
   '--require-module ts-node/register',
   '--require src/step-definitions/**/*.ts',
   '--format progress-bar',
-  '--format node_modules/cucumber-pretty'
+  //'--format node_modules/cucumber-pretty'
+  '--format json:output/test-results.json'
 ].join(' ');
 
 module.exports = {
